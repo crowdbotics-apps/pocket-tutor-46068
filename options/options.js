ca4d6001567df7c7bf6a3b8ebb12ac816e7dee98
Crowdbotics <team@crowdbotics.com>
2024-02-07 16:27:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pocket-tutor-46068",
+  url: "https://pocket-tutor-46068.botics.co",
+  api: "https://pocket-tutor-46068.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
